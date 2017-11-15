478675736c263ef0fdbb86fd392a117aeb22fb20
Mike Lazer-Walker <michael@lazerwalker.com>
2017-11-15 15:12:47 -0800

    Revert tap-the-artwork behavior

--- ArtWallsThing/ViewController.m
+++ ArtWallsThing/ViewController.m
@@ -25,7 +25,6 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, strong, nullable) UILabel *userMessagesLabel;
 
 @property (nonatomic) BOOL isReady;
-@property (nonatomic) BOOL isMoving;
 
 @property (nonatomic, strong, readonly) ARAugmentedRealityConfig *config;
 
@@ -230,20 +229,6 @@ NS_ASSUME_NONNULL_BEGIN
 }
 
 #pragma mark - Touches
-
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(nullable UIEvent *)event {
-    if (touches.count != 1) { return; } // TODO
-    UITouch *touch = [touches anyObject];
-    CGPoint point = [touch locationInView:self.sceneView];
-
-    NSArray <SCNHitTestResult *> *results = [self.sceneView hitTest:point options: nil];
-    for (SCNHitTestResult *result in results) {
-        if ([result.node isEqual:self.artwork]) {
-            self.isMoving = YES;
-        }
-    }
-}
-
 /**
  * The current implementation of moving art:
  * When you touch the screen, we check if your point hits the invisible plane
@@ -252,8 +237,9 @@ NS_ASSUME_NONNULL_BEGIN
  * This is currently pretty slow, and is a janky experience.
  */
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(nullable UIEvent *)event {
-    if (!self.isMoving) { return; }
     if (touches.count != 1) { return; } // TODO
+    if (!self.artwork) { return; }
+    
     UITouch *touch = [touches anyObject];
     CGPoint point = [touch locationInView:self.sceneView];
 
@@ -267,14 +253,6 @@ NS_ASSUME_NONNULL_BEGIN
             self.artwork.position = result.worldCoordinates;
         }
     }
-}
-
-- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(nullable UIEvent *)event {
-    self.isMoving = NO;
-}
-
-- (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(nullable UIEvent *)event {
-    self.isMoving = NO;
 }
 
 NS_ASSUME_NONNULL_END
